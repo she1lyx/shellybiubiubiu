37efa6f2dcf24a74db74782bb1c9a3006d949275
she1lyx <97996874+she1lyx@users.noreply.github.com>
2022-05-04 00:46:13 -0400

    pj3

--- Projects/Project-03/00groups/script.js
+++ Projects/Project-03/00groups/script.js
@@ -1,10 +1,10 @@
+//Search Bar
 function searchFunction() {
-    var input, filter, ul, li, a, i , txtValue;
-    input = document.getElementById('myInput');
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
-    ul = document.getElementsByClassName("grid-container-inner grid");
+    ul = document.getElementById("myUL");
     li = ul.getElementsByTagName("li");
-
     for (i = 0; i < li.length; i++) {
         a = li[i].getElementsByTagName("a")[0];
         txtValue = a.textContent || a.innerText;
@@ -15,3 +15,71 @@ function searchFunction() {
         }
     }
 }
+
+
+//Filter
+filterSelection("all")
+function filterSelection(c) {
+    var x, i;
+    x = document.getElementsByClassName("grid-item");
+    if (c == "all") c = "";
+    for (i = 0; i < x.length; i++) {
+      removeClass(x[i], "show");
+      if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
+    }
+  }
+
+  function addClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+      if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+    }
+  }
+  
+  function removeClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+      while (arr1.indexOf(arr2[i]) > -1) {
+        arr1.splice(arr1.indexOf(arr2[i]), 1);     
+      }
+    }
+    element.className = arr1.join(" ");
+  }
+
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+
+
+//Filter Button
+var button = document.getElementById("filter-button");
+var container = document.getElementById("myBtnContainer");
+var input = document.querySelectorAll("input");
+
+button.onclick = function (e) {
+  e.stopPropagation();
+  if (container.classList.contains("filters--active")) {
+    container.classList.remove("filters--active");
+  } else {
+    container.classList.add("filters--active");
+  }
+};
+
+container.onclick = function (e) {
+  e.stopPropagation();
+};
+
+window.onclick = function () {
+  container.classList.remove("filters--active");
+};
